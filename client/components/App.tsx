33c89e26b0f47a323de1e810ae9fef569d843266
achoo-o <amyj.etana@gmail.com>
2024-08-01 19:52:31 +1200

    Add basic header

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,9 @@
+import Header from './Header'
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
-  )
+  <Header />
+)
 }
 
 export default App
---
+++ client/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Testing Forms</h1>
+      <p>React development has begun!</p>
+    </div>
+  )
+}
