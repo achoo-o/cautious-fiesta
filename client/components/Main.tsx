ad36ec2e7f7126e5056d0bf8d27591e3788848cc
achoo-o <amyj.etana@gmail.com>
2024-08-05 20:26:43 +1200

    Adjust CSS align and add basic empty component

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -4,9 +4,14 @@ export default function Main() {
 
   return (
     <div className="menu-container margin30">
-      <div className="menu-item textCenter">
+      <div className="menu-item textCenter margin30">
       <Link to={'top-100-animals'}>
       <p>Top 100 Animals</p>
+      </Link>
+      </div>
+      <div className="menu-item textCenter margin30">
+      <Link to={'interesting-animals'}>
+      <p>Highlighted Animals</p>
       </Link>
       </div>
     </div>
