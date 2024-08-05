79936e7f7992328d5bb4f721f77e876372bea829
achoo-o <amyj.etana@gmail.com>
2024-08-05 20:27:20 +1200

    Add interestingAnimals route

--- client/components/router.tsx
+++ client/components/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, createRoutesFromElements, Route } from 'react-rout
 import App from './App'
 import Main from './Main'
 import Top100Animals from './Top100Animals'
+import InterestingAnimals from './InterestingAnimals'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Main />} />
     <Route path='top-100-animals' element={<Top100Animals />} />
+    <Route path='interesting-animals' element={<InterestingAnimals />} />
   </Route>
 )
 
