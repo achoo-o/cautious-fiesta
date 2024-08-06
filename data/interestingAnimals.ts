ea676d49698d260e5eb219ee31050796f1fbe60a
Amy <58102593+achoo-o@users.noreply.github.com>
2024-08-06 12:13:14 +1200

    Add description to each animal

--- data/interestingAnimals.ts
+++ data/interestingAnimals.ts
@@ -2,42 +2,50 @@ const interestingAnimals = {
   Wombat: {
     imageName: 'wombat.jpg',
     imageLink: 'https://unsplash.com/photos/girl-in-white-shirt-holding-gray-animal-tPIOleTRY5A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@davidclode?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@davidclode?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'Wombats hail from Australia and produce cube-shaped poops.'
   },
   Chinchilla: {
     imageName: 'chinchilla.jpg',
     imageLink: 'https://unsplash.com/photos/gray-rabbit-on-persons-hand-pTnsGY06GO0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@nyushasvoboda?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@nyushasvoboda?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'Chinchillas are social animals and prefer to live with a friend. They originally come from western South America.'
   },
   HoppingMouse: {
     imageName: 'hopping-mouse.jpg',
     imageLink: 'https://www.flickr.com/photos/michaelsale/5305794581/in/photolist-95RzSV-95UAUJ',
-    credit: 'https://www.flickr.com/photos/michaelsale/'
+    credit: 'https://www.flickr.com/photos/michaelsale/',
+    description: 'Referred to as an "old-world rodent", this bad little fella pees solid urine.'
   },
   Rat: {
     imageName: 'rat.jpg',
     imageLink: 'https://unsplash.com/photos/white-and-brown-hamster-on-white-surface-rNDjtoR_VRM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@rwgusev?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@rwgusev?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'Rat. Hell yeah.'
   },
   Rabbit: {
     imageName: 'rabbit.jpg',
     imageLink: 'https://unsplash.com/photos/gray-rabbit-on-brown-wood-log-302xfiIGOfE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@stuffinabox?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@stuffinabox?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'A popular pet that comes in many breeds, most of which are social.'
   },
   PallasCat: {
     imageName: 'pallas-cat.jpg',
     imageLink: 'https://unsplash.com/photos/a-cat-is-standing-on-a-tree-branch-clu5leZuGCE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@anastasiya_dal?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@anastasiya_dal?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'Look up "Pallas Cat steps on tail" to see how these fluffy cats keep their paws warm.'
   },
   Ferret: {
     imageName: 'ferret.jpg',
     imageLink: 'https://www.pexels.com/photo/white-and-brown-rodent-on-the-table-7179720/',
-    credit: 'https://www.pexels.com/@verina-37726706/'
+    credit: 'https://www.pexels.com/@verina-37726706/',
+    description: 'Unfortunately illegal to own as a pet in New Zealand. Very cute and long.'
   },
   Otter: {
     imageName: 'otter.jpg',
     imageLink: 'https://unsplash.com/photos/a-couple-of-otters-are-sleeping-together-4IkJENQk4WA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@iqsphotography?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+    credit: 'https://unsplash.com/@iqsphotography?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    description: 'Carnivorous mammals that use little rocks as tools to crack open foods.'
   },
 }
 
