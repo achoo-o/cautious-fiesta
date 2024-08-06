3b615afd798609251d63d707b601abe26e123ca8
Amy <58102593+achoo-o@users.noreply.github.com>
2024-08-06 17:25:00 +1200

    Add temp fix to list display

--- client/components/InterestingAnimals.tsx
+++ client/components/InterestingAnimals.tsx
@@ -1,37 +1,46 @@
-import { Link } from "react-router-dom"
-import interestingAnimals from "../../data/interestingAnimals"
+import { Link } from 'react-router-dom'
+import interestingAnimals from '../../data/interestingAnimals'
 
 const animalNames = Object.keys(interestingAnimals)
 
 export default function InterestingAnimals() {
-  
-
   return (
     <>
-    <div className="text-center">
-      <h2>Some Cool Animals</h2>
-      <p>Below are 8 animals I think are cool and/or cute. Click on each one to learn a little more about them!</p>
-      {animalNames.map((name, i) => {
-        let spacedName = name.match(/[A-Z][a-z]+/g)
-        if (spacedName && name !="JapaneseSpiderCrab") //JapaneseSpiderCrab is the start of the 'interesting' list. Could make this a component and prop drill.
-        return (
-        <Link to={name}>
-          <h3 key={i}>{spacedName.join(" ")}</h3>
-        </Link>)
-      })}
+      <div className="text-center">
+        <h2>Some Cool Animals</h2>
+        <p>
+          Below are 8 animals I think are cool and/or cute. Click on each one to
+          learn a little more about them!
+        </p>
+        {animalNames.map((name, i) => {
+          let spacedName = name.match(/[A-Z][a-z]+/g)
+          if (spacedName && i < 8)
+            //JapaneseSpiderCrab at index 9 is the start of the 'interesting' list. Could make this a component and prop drill.
+            return (
+              <Link to={name}>
+                <h3 key={i}>{spacedName.join(' ')}</h3>
+              </Link>
+            )
+        })}
       </div>
       <div className="text-center">
-      <h2>A Few Interesting Animals</h2>
-      <p>Here are some peculiar animals you might not know about...</p>
-      {animalNames.map((name, i) => {
-        let spacedName = name.match(/[A-Z][a-z]+/g)
-        if (spacedName && (name =="JapaneseSpiderCrab" || name == "GoblinShark" || name == "DesertRainFrog"))
-        return (
-        <Link to={name}>
-          <h3 key={i}>{spacedName.join(" ")}</h3>
-        </Link>)
-      })}
-    </div>
+        <h2>A Few Interesting Animals</h2>
+        <p>Here are some peculiar animals you might not know about...</p>
+        {animalNames.map((name, i) => {
+          let spacedName = name.match(/[A-Z][a-z]+/g)
+          if (
+            spacedName &&
+            (name == 'JapaneseSpiderCrab' ||
+              name == 'GoblinShark' ||
+              name == 'DesertRainFrog')
+          )
+            return (
+              <Link to={name}>
+                <h3 key={i}>{spacedName.join(' ')}</h3>
+              </Link>
+            )
+        })}
+      </div>
     </>
   )
 }
--- client/components/InterestingDetails.tsx
+++ client/components/InterestingDetails.tsx
@@ -1,17 +1,17 @@
-import { useParams } from "react-router-dom"
-import interestingAnimals from "../../data/interestingAnimals"
+import { useParams } from 'react-router-dom'
+import interestingAnimals from '../../data/interestingAnimals'
 
 export default function InterestingDetails() {
   const { name } = useParams()
-  const {imageName, imageLink, credit, description} = interestingAnimals[name]
-  let prettyName = name.match(/[A-Z][a-z]+/g)?.join(" ") //DesertRainFrog -> Desert Rain Frog
+  const { imageName, imageLink, credit, description } = interestingAnimals[name]
+  let prettyName = name.match(/[A-Z][a-z]+/g)?.join(' ') //DesertRainFrog -> Desert Rain Frog
   return (
     <div className="text-center">
       <h2>{prettyName}</h2>
       <img src={`/images/${imageName}`}></img>
       <p>
-      <a href={imageLink}>Image</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-      <a href={credit}>Credit</a>
+        <a href={imageLink}>Image</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+        <a href={credit}>Credit</a>
       </p>
       <p>{description}</p>
     </div>
