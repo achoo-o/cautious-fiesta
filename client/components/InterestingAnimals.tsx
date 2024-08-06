ad14659015bf54f7da3a7f6559f72c9a4d3ad2ec
Amy <58102593+achoo-o@users.noreply.github.com>
2024-08-06 12:01:32 +1200

    Move interestingAnimals object into data

--- client/components/InterestingAnimals.tsx
+++ client/components/InterestingAnimals.tsx
@@ -1,47 +1,6 @@
-const animals = {
-  Wombat: {
-    imageName: 'wombat.jpg',
-    imageLink: 'https://unsplash.com/photos/girl-in-white-shirt-holding-gray-animal-tPIOleTRY5A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@davidclode?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-  Chinchilla: {
-    imageName: 'chinchilla.jpg',
-    imageLink: 'https://unsplash.com/photos/gray-rabbit-on-persons-hand-pTnsGY06GO0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@nyushasvoboda?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-  HoppingMouse: {
-    imageName: 'hopping-mouse.jpg',
-    imageLink: 'https://www.flickr.com/photos/michaelsale/5305794581/in/photolist-95RzSV-95UAUJ',
-    credit: 'https://www.flickr.com/photos/michaelsale/'
-  },
-  Rat: {
-    imageName: 'rat.jpg',
-    imageLink: 'https://unsplash.com/photos/white-and-brown-hamster-on-white-surface-rNDjtoR_VRM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@rwgusev?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-  Rabbit: {
-    imageName: 'rabbit.jpg',
-    imageLink: 'https://unsplash.com/photos/gray-rabbit-on-brown-wood-log-302xfiIGOfE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@stuffinabox?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-  PallasCat: {
-    imageName: 'pallas-cat.jpg',
-    imageLink: 'https://unsplash.com/photos/a-cat-is-standing-on-a-tree-branch-clu5leZuGCE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@anastasiya_dal?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-  Ferret: {
-    imageName: 'ferret.jpg',
-    imageLink: 'https://www.pexels.com/photo/white-and-brown-rodent-on-the-table-7179720/',
-    credit: 'https://www.pexels.com/@verina-37726706/'
-  },
-  Otter: {
-    imageName: 'otter.jpg',
-    imageLink: 'https://unsplash.com/photos/a-couple-of-otters-are-sleeping-together-4IkJENQk4WA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
-    credit: 'https://unsplash.com/@iqsphotography?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
-  },
-}
+import interestingAnimals from "../../data/interestingAnimals"
 
-const animalNames = Object.keys(animals)
+const animalNames = Object.keys(interestingAnimals)
 
 export default function InterestingAnimals() {
   
