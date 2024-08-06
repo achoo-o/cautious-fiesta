6ec39d65c2ccecad0092d76effbacad8fd185f2c
Amy <58102593+achoo-o@users.noreply.github.com>
2024-08-06 12:45:41 +1200

    Add images - image link currently broken

--- client/components/InterestingDetails.tsx
+++ client/components/InterestingDetails.tsx
@@ -1,3 +1,14 @@
+import { useParams } from "react-router-dom"
+import interestingAnimals from "../../data/interestingAnimals"
+
 export default function InterestingDetails() {
-  return <p>Hello</p>
+  const { name } = useParams()
+  const {imageName, imageLink, link, credit, description} = interestingAnimals[name]
+  console.log(`../images/${imageName}`)
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={`../images/${imageName}`}></img>
+    </div>
+  )
 }
