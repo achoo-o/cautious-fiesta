9537c349c66f2a8a5782c802a0045e7c8d3d7502
achoo-o <amyj.etana@gmail.com>
2024-08-07 21:56:45 +1200

    Display date and location of each event

--- client/components/RoadEvents.tsx
+++ client/components/RoadEvents.tsx
@@ -12,12 +12,20 @@ export default function RoadEvents() {
   if (isPending) return (<p>Loading...</p>)
 
   if (isError) return (<p>{`Failed to retrieve data: ${error.message}`}</p>)
-  
+
   return (
     <div>
-      {data.features.map((el, i) => {
+      {data.features.map((el) => {
+        const timestamp = el.properties.startDate
+        const timestampToDate = new Date(timestamp)
+        const dateArr = timestampToDate.toString().split('')
+        const date = dateArr.splice(0, 10).join('') + ' ' + dateArr.splice(6,5).join('')
+
         return (
-          <p key={i}>{`${el}`}</p>
+          <div key={el.properties.GlobalID}>
+            <p>{`${date}`}</p>
+            <p>{`${el.properties.locationArea}`}</p>
+          </div>
         )
       })}
     </div>
