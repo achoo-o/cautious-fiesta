7fd8b46e2b8a5186f4db2b3a9f19620bc3fac1f6
achoo-o <amyj.etana@gmail.com>
2024-08-17 10:29:24 +1200

    Overwrite main with local repo

--- client/components/apis/wakaKotahi.ts
+++ client/components/apis/wakaKotahi.ts
@@ -1,11 +1,10 @@
 import request from 'superagent'
-import type { RoadEvents } from "../../../models/road-events";
 
 //Generic Link: https://opendata-nzta.opendata.arcgis.com/datasets/NZTA::road-events/explore
 
 export async function fetchRoadEvents() {
   const res = await request.get(`/api/v1/roadevents`)
-  console.log('res', res)
+  console.log('res', res.body.type)
   // return res.body as RoadEvents
   return res.body
 }
